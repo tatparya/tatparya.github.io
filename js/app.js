865695303fde77bda0a0e5a96583d89f6813a4f1
tatparya <tatparya.shankar@gmail.com>
2015-05-13 04:28:00 -0400

    Added About and contact info

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
-var name = "Tatparya Shankar"
-var tag = "Engineer | Programmer | Designer"
+var name = "Tatparya Shankar";
+var tag = "Engineer | Programmer | Designer";
+
 $(".brand-heading").append( name );
 $(".intro-text").append( tag );
+
+var aboutMe = "I am Tatparya Shankar, currently pursuing an \
+undergraduate degree in the field of Computer Engineering \
+from Purdue University. A very passionate and quick learner \
+with a strong base of object oriented programming. I am very \
+passionate about android development, web development, \
+robotics and graphic designing. I am seeking a challenging \
+work environment and a position of responsibility that would \
+allow me to utilize and further refine my technical and \
+interpersonal skills and also enrich my knowledge.";
+
+$("#about").append( aboutMe );
