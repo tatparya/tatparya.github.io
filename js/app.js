c9098b63f4e164bde73ad7d3c71b2b7d090a6696
tatparya <tatparya.shankar@gmail.com>
2015-05-14 07:02:59 -0400

    Added nav, resume

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 var initInfo = function(){
 
+    //  Populating Header
     var name = "Tatparya Shankar";
     var tag = "Engineer | Programmer | Designer";
 
@@ -15,22 +16,5 @@ var initInfo = function(){
     work environment and a position of responsibility that would \
     allow me to utilize and further refine my technical and \
     interpersonal skills and also enrich my knowledge.";
-
-    var interests = ["Aritificial Intelligence", "Machine Learning",
-     "Web Development", "Security", "Mobile App Development"];
-
-    var skills = ["C++", "C#", "Python", "Java", "HTML", "JavaScript" ];
-
-    $("#about-text").append( aboutMe );
-
-    for ( i in interests )
-    {
-        $("#interests").append( "<li>" + interests[ i ] + "</li>" );
-    }
-
-    for ( j in skills )
-    {
-        $("#skills").append( "<li>" + skills[ j ] + "</li>" );
-    }
 
 }
