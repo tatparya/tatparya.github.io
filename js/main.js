9534b862054119f711cc5398bb4067ecdb04ab35
tatparya <tatparya.shankar@gmail.com>
2015-05-15 18:05:19 -0400

    Added loader

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+
+    setTimeout( function(){
+        //  Fade out loader
+        $(".loader").fadeOut( 2000, "easeInOutExpo" );
+    }, 6000);
+
     $(window).scroll(function(e){
         navFn();
     })
