1cbc6045e529bfe976b2c20c2d6783af543422c1
tatparya <tatparya.shankar@gmail.com>
2015-05-15 17:25:43 -0400

    Done, not optimized for phone

--- js/form.js
+++ js/form.js
@@ -4,5 +4,14 @@ $( function() {
         navigate( this );
         $("#email-form").slideDown(4000, "easeInOutExpo");
 
-    })
+    });
+
+    $("button.btn-send").click( function() {
+
+        //  Hide Form and Add Thank you
+        $("#email-form").slideUp(3000, "easeInOutExpo");
+        $("button.form-reveal").slideUp( 2000, "easeInOutExpo");
+
+        $(".thanks").slideDown(5000, "easeInOutExpo" );
+    });
 })
