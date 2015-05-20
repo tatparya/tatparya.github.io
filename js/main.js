5060a4bbab34a656882f6f5a8f3ec474a4aa1315
tatparya <tatparya.shankar@gmail.com>
2015-05-20 04:16:44 -0400

    Fixed page loading time

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     setTimeout( function(){
         //  Fade out loader
         $(".loader").fadeOut( 2000, "easeInOutExpo" );
-    }, 0);
+    }, 3000);
 
     $(window).scroll(function(e){
         navFn();
